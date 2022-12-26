e69f3c6a3cbe483ad1406532019fa970b77c9b0c
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2022-12-26 19:07:44 +0100

    .

---
+++ Html/html.js
@@ -0,0 +1,36 @@
+import Head from "next/head"
+
+export default function () {
+    return (
+
+        <div>
+            <Head>
+                <link rel="shortcut icon" href="/white.jpg" />
+                <title>Mejor Tech</title>
+
+                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+
+                <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,100&display=swap" rel="stylesheet"></link>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet" />
+
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300&display=swap" rel="stylesheet" />
+
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+                <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200&display=swap" rel="stylesheet"></link>
+
+
+                <script src="https://s.pageclip.co/v1/pageclip.js" charset="utf-8"></script>
+            </Head>
+
+
+        </div>
+    )
+}
+
+
+
