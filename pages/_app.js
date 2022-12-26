f8987ab1c23e80a0f8e44aaf8078abb85b358e01
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2022-12-26 20:19:09 +0100

    Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
+import Head from "../Html/html"
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <div className='Container'>
-
+      <Head />
       <Component {...pageProps} />
-      
     </div>
   )
 }
