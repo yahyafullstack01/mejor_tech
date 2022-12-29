c9bd43dfc2ec71bb25fe93f70b4b334143f123b4
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2022-12-29 20:00:50 +0100

    third testing

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -3,7 +3,7 @@ import { BsGlobe } from 'react-icons/bs';
 import Link from "next/link";
 
 export default function header() {
-    
+
     return (
         <div className={styles.container}>
             <img src="/logo.png"
@@ -17,7 +17,7 @@ export default function header() {
                 <Link passHref href="" className="text-xl text-white">Contact Us</Link>
             </nav>
             <div className="flex items-center ">
-                <BsGlobe className="text-xl text-white "/>
+                <BsGlobe className="text-xl text-white " />
                 <select className="bg-transparent text-white outline-none">
                     <option className="" value="en">English</option>
                     <option value="ab">Arabic</option>
