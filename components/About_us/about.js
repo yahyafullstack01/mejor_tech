8c34f87c4c05a1a3f624fd66c92a4f6d9025e430
yahya alloucha <yahyafullstack01@gmail.com>
2022-12-30 18:49:08 +0100

    class css with tailwind

--- components/About_us/about.js
+++ components/About_us/about.js
@@ -1,8 +1,8 @@
-
+import styles from "../About_us/about.module.css";
 export default function About() {
     
     return (
-        <div className="Container bg-gray-300">
+        <div className={`${styles.container} ${"items-center justify-center bg-slate-400 h-screen"}`}>
             hello every one
         </div>
     )
