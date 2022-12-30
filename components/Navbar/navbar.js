4e4a8f0a9292714eda759a7e3917ef45889ecbf3
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2022-12-30 12:01:52 +0100

    Header and navbar finished

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -1,29 +1,67 @@
 import styles from "./navbar.module.css"
 import { BsGlobe } from 'react-icons/bs';
+import { TbEqual } from 'react-icons/tb';
+import { RxCross1 } from 'react-icons/rx';
 import Link from "next/link";
 
 export default function header() {
 
     return (
-        <div className={styles.container}>
-            <img src="/logo.png"
-                alt="The Logo of the company"
-                className="w-36"
-            />
-            <nav className="flex w-96 justify-between">
-                <Link passHref href="" className="text-xl text-white">About</Link>
-                <Link passHref href="" className="text-xl text-white">Our Work</Link>
-                <Link passHref href="" className="text-xl text-white">Team</Link>
-                <Link passHref href="" className="text-xl text-white">Contact Us</Link>
+
+        <div className={`${styles.container} ${"items-center justify-between flex"}`}>
+            <Link passHref href="/">
+                <img
+                    src="/logo.png"
+                    alt="The Logo of the company"
+                    className="w-24 md:w-32	"
+                />
+            </Link>
+            <nav className={"sm:flex md:w-96 w-72 justify-between hidden"}>
+                <Link passHref href="" className={`${styles.navigation} ${"text-sm md:text-xl  text-white"}`}>About</Link>
+                <Link passHref href="" className={`${styles.navigation} ${"text-sm md:text-xl  text-white"}`}>Our Work</Link>
+                <Link passHref href="" className={`${styles.navigation} ${"text-sm md:text-xl  text-white"}`}>Team</Link>
+                <Link passHref href="" className={`${styles.navigation} ${"text-sm md:text-xl  text-white"}`}>Contact Us</Link>
             </nav>
-            <div className="flex items-center ">
+            <div className="sm:flex items-center md:text-lg hidden ">
                 <BsGlobe className="text-xl text-white " />
-                <select className="bg-transparent text-white outline-none">
-                    <option className="" value="en">English</option>
+                <select className={`${styles.selection} ${"bg-transparent text-white outline-none"}`}>
+                    <option value="en">English</option>
                     <option value="ab">Arabic</option>
                     <option value="ru">Russian</option>
                 </select>
             </div>
+            <a href="#menu" className="sm:hidden flex">
+                <TbEqual className="sm:hidden text-4xl text-white flex" />
+            </a>
+            <section id="menu" className={styles.menu}>
+                <div className={`${styles.bar} ${"flex items-center justify-between"}`}>
+                    <a href="#">
+                        <img
+                            src="/logo.png"
+                            alt="The Logo of the company"
+                            className="w-32"
+                        />
+                    </a>
+                    <a href="#">
+                        <RxCross1 className="text-4xl text-white flex" />
+                    </a>
+                </div>
+
+                <div className="flex items-center justify-center flex-col gap-y-12 mt-2">
+                    <Link passHref href="" className="text-2xl text-center text-white  w-full">About</Link>
+                    <Link passHref href="" className="text-2xl text-center text-white  w-full">Our Work</Link>
+                    <Link passHref href="" className="text-2xl text-center text-white  w-full">Team</Link>
+                    <Link passHref href="" className="text-2xl text-center text-white  w-full">Contact Us</Link>
+                </div>
+                <div className="flex w-full justify-center mt-10 items-center text-lg  ">
+                    <BsGlobe className="text-xl text-white " />
+                    <select className={`${styles.selection} ${"bg-transparent text-white outline-none"}`}>
+                        <option value="en">English</option>
+                        <option value="ab">Arabic</option>
+                        <option value="ru">Russian</option>
+                    </select>
+                </div>
+            </section>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Navbar from "../components/Navbar/navbar";
 import Intro from "../components/Introduction/intro";
 import About from "../components/About_us/about";
+import Footer from "../components/Footer/footer";
+
 export default function Home() {
-  return(
-  <div className="">
-    <Navbar/>
-    <Intro/>
-    <About/>
-  </div>
+  return (
+    <div className="">
+      <Navbar />
+      <Intro />
+      <About />
+      <Footer/>
+    </div>
   )
 }
